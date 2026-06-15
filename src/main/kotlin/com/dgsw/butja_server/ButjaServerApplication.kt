b378c8cdbb1f576e7e01497cf39e414ce4c264d9
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-06-15 13:43:02 +0900

    feat: BaseTimeEntity

--- src/main/kotlin/com/dgsw/butja_server/ButjaServerApplication.kt
+++ src/main/kotlin/com/dgsw/butja_server/ButjaServerApplication.kt
@@ -3,7 +3,9 @@ package com.dgsw.butja_server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class ButjaServerApplication
--- src/main/kotlin/com/dgsw/butja_server/global/common/entity/BaseTimeEntity.kt
+++ src/main/kotlin/com/dgsw/butja_server/global/common/entity/BaseTimeEntity.kt
@@ -10,7 +10,6 @@ import java.time.LocalDateTime
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity {
-
     @CreatedDate
     var createdAt: LocalDateTime? = null
         protected set
